9016745749b3a1e68970d67333c33549c18ed202
ClementPla <clement.playout@polymt.ca@users.noreply.github.com>
2023-07-07 00:11:17 +0200

    Fix import

--- fundus_lesions_toolkit/__init__.py
+++ fundus_lesions_toolkit/__init__.py
@@ -1,4 +1,1 @@
-from .utils import *
-from .models import segmentation, checkpoints
-from .data import *
-from .losses import *
+from . import *
--- fundus_lesions_toolkit/models/__init__.py
+++ fundus_lesions_toolkit/models/__init__.py
@@ -1,4 +1,6 @@
 from .checkpoints import DOWNLOADABLE_MODELS
+from .segmentation import segment, batch_segment
+
 
 def list_models():
     return list(DOWNLOADABLE_MODELS.keys())
