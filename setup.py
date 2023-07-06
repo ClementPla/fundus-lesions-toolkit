8f6f3c6671d1a780d1f122544ff7ab11c2c84c41
ClementPla <clement.playout@polymt.ca@users.noreply.github.com>
2023-07-06 19:49:57 +0200

    Fix setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'fundus_lesions_toolkit',         # How you named your package folder (MyLib)
   packages = ['fundus_lesions_toolkit'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.1.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Tools Lesions Segmentation in Fundus',   # Give a short description about your library
   author = 'Clement Playout',                   # Type in your name
@@ -10,11 +10,11 @@ setup(
   url = 'https://github.com/ClementPla/fundus-lesions-toolkit/tree/main',   # Provide either the link to your github or to your website
   download_url = 'https://github.com/ClementPla/fundus-lesions-toolkit/archive/refs/tags/v_01.tar.gz',    # I explain this later on
   keywords = ['Segmentation', 'Fundus', 'Pytorch', 'Models', 'Semantic', 'Lesions'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
+  install_requires=[            
           'pytorch',
           'nntools',
           'torchvision',
-          'cv2',
+          'opencv-python',
           'numpy',
           'matplotlib'
       ],
@@ -22,7 +22,6 @@ setup(
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
-  ],
+    'License :: OSI Approved :: MIT License', 
+    'Programming Language :: Python :: 3',]
 )
